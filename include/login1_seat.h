94f73e44570aa356b25fc7b87a6a10b58633c931
xzl <1037981455@qq.com>
2022-07-13 13:20:24 +0800

    feat:add login1_seat

--- include/login1_seat.h
+++ include/login1_seat.h
@@ -16,6 +16,43 @@ public:
     explicit Login1Seat(const QString &path, QObject *parent = nullptr);
     virtual ~Login1Seat();
 
+    Q_PROPERTY(QList<SessionPath> Sessions READ sessions NOTIFY SessionsChanged)
+    Q_PROPERTY(bool CanGraphical READ canGraphical  NOTIFY CanGraphicalChanged)
+    Q_PROPERTY(bool CanTTY READ canTTY NOTIFY CanTTYChanged)
+    Q_PROPERTY(bool IdleHint READ idleHint NOTIFY IdleHintChanged)
+    Q_PROPERTY(QString Id READ id NOTIFY IdChanged)
+    Q_PROPERTY(SessionPath ActiveSession READ activeSession NOTIFY ActiveSessionChanged)
+    Q_PROPERTY(quint64 IdleSinceHint READ idleSinceHint NOTIFY IdleSinceHintChanged)
+    Q_PROPERTY(quint64 IdleSinceHintMonotonic READ idleSinceHintMonotonic NOTIFY IdleSinceHintMonotonicChanged)
+
+    bool canGraphical();
+    bool canTTY();
+    bool idleHint();
+    QList<SessionPath> sessions();
+    QString id();
+    SessionPath activeSession();
+    quint64 idleSinceHint();
+    quint64 idleSinceHintMonotonic();
+
+signals:
+    void errorMessageChanged(const QString &message);
+// properties changed
+    void SessionsChanged(const QList<SessionPath> &value);
+    void CanGraphicalChanged(const bool value);
+    void CanTTYChanged(const bool value);
+    void IdleHintChanged(const bool value);
+    void IdChanged(const QString &value);
+    void ActiveSessionChanged(const SessionPath &value);
+    void IdleSinceHintChanged(const quint64 value);
+    void IdleSinceHintMonotonicChanged(const quint64 value);
+
+public slots:
+    void activateSession(const QString & session_id);
+    void switchTo(const uint vtnr);
+    void switchToNext();
+    void switchToPrevious();
+    void terminate();
+
 private:
     QScopedPointer<Login1SeatPrivate> d_ptr;
     Q_DECLARE_PRIVATE(Login1Seat)
--- src/login1_seat.cpp
+++ src/login1_seat.cpp
@@ -3,6 +3,7 @@
 #include "dbusinterface.h"
 #include "login1_seat_p.h"
 #include <qobject.h>
+#include <qdbuspendingreply.h>
 
 LOGIN1_BEGIN_NAMESPACE
 Login1Seat::Login1Seat(const QString &path, QObject *parent)
@@ -14,6 +15,111 @@ Login1Seat::Login1Seat(const QString &path, QObject *parent)
 
     Q_D(Login1Seat);
     d->m_inter = new DBusInterface(Service, path, Interface, QDBusConnection::systemBus(), this);
+}
+
+QList<SessionPath> Login1Seat::sessions()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<QList<SessionPath>>(d->m_inter->property("Sessions"));
+}
+
+bool Login1Seat::canGraphical()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<bool>(d->m_inter->property("CanGraphical"));
+}
+
+bool Login1Seat::canTTY()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<bool>(d->m_inter->property("CanTTY"));
+}
+
+bool Login1Seat::idleHint()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<bool>(d->m_inter->property("IdleHint"));
+}
+
+QString Login1Seat::id()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<QString>(d->m_inter->property("Id"));
+}
+
+SessionPath Login1Seat::activeSession()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<SessionPath>(d->m_inter->property("ActiveSession"));
+}
+
+quint64 Login1Seat::idleSinceHint()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<quint64>(d->m_inter->property("IdleSinceHint"));
+}
+
+quint64 Login1Seat::idleSinceHintMonotonic()
+{
+    Q_D(Login1Seat);
+    return qvariant_cast<quint64>(d->m_inter->property("IdleSinceHintMonotonic"));
+}
+
+void Login1Seat::activateSession(const QString &session_id)
+{
+    Q_D(Login1Seat);
+    QVariantList args {QVariant::fromValue(session_id)};
+    QDBusPendingReply<> replay = d->m_inter->asyncCallWithArgumentList("ActivateSession", args);
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
+}
+
+void Login1Seat::switchTo(const uint vtnr)
+{
+    Q_D(Login1Seat);
+    QDBusPendingReply<> replay = d->m_inter->asyncCallWithArgumentList(QStringLiteral("SwitchTo"),
+        {QVariant::fromValue(vtnr)});
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
+}
+
+void Login1Seat::switchToNext()
+{
+    Q_D(Login1Seat);
+    QDBusPendingReply<> replay = d->m_inter->asyncCall(QStringLiteral("SwitchToNext"));
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
+}
+
+void Login1Seat::switchToPrevious()
+{
+    Q_D(Login1Seat);
+    QDBusPendingReply<> replay = d->m_inter->asyncCall(QStringLiteral("SwitchToPrevious"));
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
+}
+
+void Login1Seat::terminate()
+{
+    Q_D(Login1Seat);
+    QDBusPendingReply<> replay = d->m_inter->asyncCall(QStringLiteral("Terminate"));
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
 }
 
 Login1Seat::~Login1Seat() {}
