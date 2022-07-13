a647df7cf30a71430089bf0db7a06b30715379b2
guoyao <guoyao@uniontech.com>
2022-07-13 13:59:41 +0800

    chore: user interface done

--- include/login1_user.h
+++ include/login1_user.h
@@ -16,6 +16,61 @@ public:
     explicit Login1User(const QString &path, QObject *parent = nullptr);
     virtual ~Login1User();
 
+    Q_PROPERTY(QList<SessionPath> Sessions READ sessions NOTIFY SessionsChanged)
+    Q_PROPERTY(bool IdleHint READ idleHint NOTIFY IdleHintChanged)
+    Q_PROPERTY(bool Linger READ linger NOTIFY LingerChanged)
+    Q_PROPERTY(QString Name READ name NOTIFY NameChanged)
+    Q_PROPERTY(QString RuntimePath READ runtimePath NOTIFY RuntimePathChanged)
+    Q_PROPERTY(QString Service READ service NOTIFY ServiceChanged)
+    Q_PROPERTY(QString Slice READ slice NOTIFY SliceChanged)
+    Q_PROPERTY(QString State READ state NOTIFY StateChanged)
+    Q_PROPERTY(SessionPath Display READ display NOTIFY DisplayChanged)
+    Q_PROPERTY(uint GID READ GID NOTIFY GIDChanged)
+    Q_PROPERTY(uint UID READ UID NOTIFY UIDChanged)
+    Q_PROPERTY(quint64 IdleSinceHint READ idleSinceHint NOTIFY IdleSinceHintChanged)
+    Q_PROPERTY(quint64 IdleSinceHintMonotonic READ idleSinceHintMonotonic NOTIFY IdleSinceHintMonotonicChanged)
+    Q_PROPERTY(quint64 Timestamp READ timestamp NOTIFY TimestampChanged)
+    Q_PROPERTY(quint64 TimestampMonotonic READ timestampMonotonic NOTIFY TimestampMonotonicChanged)
+
+    QList<SessionPath> sessions();
+    bool idleHint();
+    bool linger();
+    QString name();
+    QString runtimePath();
+    QString service();
+    QString slice();
+    QString state();
+    SessionPath display();
+    uint GID();
+    uint UID();
+    quint64 idleSinceHint();
+    quint64 idleSinceHintMonotonic();
+    quint64 timestamp();
+    quint64 timestampMonotonic();
+
+signals:
+    void errorMessageChanged(const QString &message);
+    void SessionsChanged(const QList<SessionPath> &sessions);
+    void IdleHintChanged(const bool value);
+    void LingerChanged(const bool value);
+    void NameChanged(const QString &name);
+    void RuntimePathChanged(const QString &path);
+    void ServiceChanged(const QString &service);
+    void SliceChanged(const QString &slice);
+    void StateChanged(const QString &state);
+    void DisplayChanged(const SessionPath &path);
+    void GIDChanged(const uint gid);
+    void UIDChanged(const uint uid);
+    void IdleSinceHintChanged(const quint64 value);
+    void IdleSinceHintMonotonicChanged(const quint64 value);
+    void TimestampChanged(const quint64 value);
+    void TimestampMonotonicChanged(const quint64 value);
+
+public slots:
+    QString lastError();
+    void kill(const int signal_number);
+    void terminate();
+
 private:
     QScopedPointer<Login1UserPrivate> d_ptr;
     Q_DECLARE_PRIVATE(Login1User)
--- src/login1_user.cpp
+++ src/login1_user.cpp
@@ -1,8 +1,11 @@
 #include "login1_user.h"
 
 #include "dbusinterface.h"
+#include "login1_types.h"
 #include "login1_user_p.h"
+#include <qlist.h>
 #include <qobject.h>
+#include <qdbuspendingreply.h>
 
 LOGIN1_BEGIN_NAMESPACE
 Login1User::Login1User(const QString &path, QObject *parent)
@@ -12,9 +15,133 @@ Login1User::Login1User(const QString &path, QObject *parent)
     const QString &Service = QStringLiteral("org.freedesktop.login1");
     const QString &Interface = QStringLiteral("org.freedesktop.login1.Seat");
 
+    SessionPath::registerMetaType();
+
     Q_D(Login1User);
     d->m_inter = new DBusInterface(Service, path, Interface, QDBusConnection::systemBus(), this);
 }
 
 Login1User::~Login1User() {}
+
+QList<SessionPath> Login1User::sessions()
+{
+    Q_D(Login1User);
+    return qvariant_cast<QList<SessionPath>>(d->m_inter->property("Sessions"));
+}
+
+bool Login1User::idleHint()
+{
+    Q_D(Login1User);
+    return qvariant_cast<bool>(d->m_inter->property("IdleHint"));
+}
+
+bool Login1User::linger()
+{
+    Q_D(Login1User);
+    return qvariant_cast<bool>(d->m_inter->property("Linger"));
+}
+
+QString Login1User::name()
+{
+    Q_D(Login1User);
+    return qvariant_cast<QString>(d->m_inter->property("Name"));
+}
+
+QString Login1User::runtimePath()
+{
+    Q_D(Login1User);
+    return qvariant_cast<QString>(d->m_inter->property("RuntimePath"));
+}
+
+QString Login1User::service()
+{
+    Q_D(Login1User);
+    return qvariant_cast<QString>(d->m_inter->property("Service"));
+}
+
+QString Login1User::slice()
+{
+    Q_D(Login1User);
+    return qvariant_cast<QString>(d->m_inter->property("Slice"));
+}
+
+QString Login1User::state()
+{
+    Q_D(Login1User);
+    return qvariant_cast<QString>(d->m_inter->property("State"));
+}
+
+SessionPath Login1User::display()
+{
+    Q_D(Login1User);
+    return qvariant_cast<SessionPath>(d->m_inter->property("Display"));
+}
+
+uint Login1User::GID()
+{
+    Q_D(Login1User);
+    return qvariant_cast<uint>(d->m_inter->property("GID"));
+}
+
+uint Login1User::UID()
+{
+    Q_D(Login1User);
+    return qvariant_cast<uint>(d->m_inter->property("UID"));
+}
+
+quint64 Login1User::idleSinceHint()
+{
+    Q_D(Login1User);
+    return qvariant_cast<quint64>(d->m_inter->property("IdleSinceHint"));
+}
+
+quint64 Login1User::idleSinceHintMonotonic()
+{
+    Q_D(Login1User);
+    return qvariant_cast<quint64>(d->m_inter->property("IdleSinceHintMonotonic"));
+}
+
+quint64 Login1User::timestamp()
+{
+    Q_D(Login1User);
+    return qvariant_cast<quint64>(d->m_inter->property("Timestamp"));
+}
+
+quint64 Login1User::timestampMonotonic()
+{
+    Q_D(Login1User);
+    return qvariant_cast<quint64>(d->m_inter->property("TimestampMonotonic"));
+}
+
+// public slots
+
+QString Login1User::lastError()
+{
+    Q_D(Login1User);
+    return d->m_errorMessage;
+}
+
+void Login1User::kill(const int signal_number)
+{
+    Q_D(Login1User);
+    QVariantList args {QVariant::fromValue(signal_number)};
+    QDBusPendingReply<> replay = d->m_inter->asyncCallWithArgumentList("Kill", args);
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
+}
+
+void Login1User::terminate()
+{
+    Q_D(Login1User);
+    QDBusPendingReply<> replay = d->m_inter->asyncCall("Terminate");
+    replay.waitForFinished();
+    if (!replay.isValid()) {
+        d->m_errorMessage = replay.error().message();
+        emit errorMessageChanged(d->m_errorMessage);
+    }
+}
+
 LOGIN1_END_NAMESPACE
