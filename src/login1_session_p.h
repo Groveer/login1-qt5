b9a169369253a281fef78c7d0847990d166ae9f2
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-07-14 11:10:54 +0800

    feat(session): add session

---
+++ src/login1_session_p.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "include/namespace.h"
+#include <qobject.h>
+
+class DBusInterface;
+LOGIN1_BEGIN_NAMESPACE
+class Login1Session;
+
+class Login1SessionPrivate : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Login1SessionPrivate(Login1Session *parent = nullptr)
+        : q_ptr(parent)
+    {
+    }
+
+    QString m_errorMessage;
+    DBusInterface *m_inter;
+    Login1Session *q_ptr;
+    Q_DECLARE_PUBLIC(Login1Session)
+};
+
+LOGIN1_END_NAMESPACE
